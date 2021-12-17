9be0a800985879d80d5e52aed3c02abb16c97f84
Lee Morin <42477710+LeeMorinUCF@users.noreply.github.com>
2021-12-17 16:34:25 -0500

    Modified Demos from ECP3004

--- demo_13_Reading_Data/PP_Ch_10B.py
+++ demo_13_Reading_Data/PP_Ch_10B.py
@@ -343,7 +343,7 @@ with open('lynx.dat', 'r') as input_file:
 
 
 
-# Maybe now you are convinced that the first one is simple. 
+# Maybe now you are convinced that the first version is simple. 
 
 
 ##################################################
@@ -537,7 +537,7 @@ def read_all_molecules(reader: TextIO) -> list:
 # This new version of read_all_molecules begins by 
 # reading the first line of the file. 
 # If the string is not empty (that is, if the file is not empty)
-# it passes both the file and the4 line into read_molecule. 
+# it passes both the file and the line into read_molecule. 
 # The new version of read_molecule then has to return two things:
 # the next molecule in the file and 
 # the first line immediately after the end of that molecule
@@ -549,13 +549,14 @@ def read_all_molecules(reader: TextIO) -> list:
 # Then, it reads lines from reader one at a time, 
 # looking for one of three situations:
 #   - The end of the file, which signals the end of both the molecule and the file.
-#   - Another CMPND line, which signals the end of this molecule 
+#   - Another COMPND line, which signals the end of this molecule 
 #   and the start of the next one. 
 #   - An ATOM, which is to be added to the current molecule. 
   
 # The key feature of this revised function is that
 # it returns *both* the molecule *and* the next line, 
 # so that the caller can keep processing. 
+
 
 
 
@@ -610,7 +611,12 @@ print(molecules)
 # Now our module handles all the cases. 
 
 
-
+# The result is probably the most complicated function we have seen so far.
+# The extra effort is worth it because the ability to work with 
+# unstructured data is highly valued in the field of business analytics:
+# any analyst can use data that are already organized
+# but those who can create organized data from an arbitrary unstructured source
+# can create something that does not yet exist. 
 
 
 
