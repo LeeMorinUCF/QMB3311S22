b4f437d6c33ed5863c9789e8e8aee3bc1b239eb1
Lee Morin <42477710+LeeMorinUCF@users.noreply.github.com>
2022-02-09 19:37:48 -0500

    Uploaded Assignment 4

---
+++ assignment_04/my_A4_functions.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+##################################################
+#
+# QMB 3311: Python for Business Analytics
+#
+# Name: 
+#
+# Date:
+# 
+##################################################
+#
+# Sample Script for Assignment 4: 
+# Function Definitions
+#
+##################################################
+"""
+
+
+##################################################
+# Import Required Modules
+##################################################
+
+# import name_of_module
+
+
+##################################################
+# Function Definitions
+##################################################
+
+# Only function definitions here - no other calculations. 
+
+# Exercise 1
+
+
+# Exercise 2
+
+
+# Exercise 3
+
+
+def logit_like_grad(y: list, x: list, beta_0: float, beta_1: float) -> float:
+    """Calculates the gradient vector of the likelihood function
+    for the bivariate logistic regression model
+    for sevaral pairs of observations in the lists x and y,
+    coefficients beta_0 and beta_1.
+    
+    Notice if you are missing the space after the >>>, 
+    it causes an error.
+    Also, an example without the >>> does not get run with doctest.
+    
+    >>> logit_like_grad([1, 1, 0, 0], [15.0, 5.0, 15.0, 5.0], 0.0, 0.0)
+    [0.0, 0.0]
+    >>> logit_like_grad([1, 1, 0, 0], [15.0, 5.0, 15.0, 5.0], math.log(3), 0.0)
+    [-1.0, -10.0]
+    >>> logit_like_grad([1, 1, 0, 0], [15.0, 5.0, 15.0, 5.0], math.log(7), 0.0)
+    [-1.5, -15.0]
+    >>> logit_like_grad([1, 0, 1], [1, 1, 1], 0.0, math.log(2))
+    [0.0, 0.0]
+    >>> logit_like_grad([1, 0, 1], [1, 1, 1], 0.0, math.log(5))
+    [-0.5, -0.5]
+    >>> logit_like_grad([1, 0, 1], [3, 3, 3], 0.0, math.log(2))
+    [-2/3, -2.0]
+    """
+    
+    return None
+
+
+# Exercise 4
+
+
+
+# Only function definitions above this point. 
+
+
+##################################################
+# Test the examples in your docstrings
+##################################################
+
+
+# Question 2: Test using the doctest module. 
+
+
+# Make sure to include exampes in your docstring
+# with the proper formatting. 
+
+# Test all functions with three examples each. 
+
+# Choose good examples that will test interesting cases. 
+# Make sure they all work correctly. 
+
+
+
+
+##################################################
+# End
+##################################################
